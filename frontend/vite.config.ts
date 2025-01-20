fb7cfb7d7872b252f4b4c84baa4d347cf097ab05
Nimrod Schatzman <nimrodschatzman5@gmail.com>
2025-01-20 16:48:52 +0200

    Update Vite config for public output in backend/dist

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: "./",
-  plugins: [react(),],
+  plugins: [react()],
   server: {
     watch: {
       usePolling: true, // Use polling for file changes if necessary
@@ -11,14 +11,14 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: '../backend/public',
+    outDir: '../backend/dist/public', // Output to backend/dist/public
     sourcemap: true, // Enable source maps for debugging
     emptyOutDir: true, // Clears the directory before building
     rollupOptions: {
-    input: '/src/main.tsx', // Ensure the entry point is correct
+      input: '/src/main.tsx', // Ensure the entry point is correct
       output: {
-        entryFileNames: `[name].jsx`, // Generate files with .js extensions
-        chunkFileNames: `[name]-[hash].jsx`,
+        entryFileNames: `[name].js`, // Ensure the correct file extension
+        chunkFileNames: `[name]-[hash].js`,
         assetFileNames: `[name]-[hash][extname]`,
       },
     },
