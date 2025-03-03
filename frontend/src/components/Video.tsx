04c38a72a13732d1a147839e5f7894e39f23968e
Nimrod Schatzman <nimrodschatzman5@gmail.com>
2025-03-03 21:47:27 +0200

    Prepare for Heroku deployment

--- frontend/src/components/Video.tsx
+++ frontend/src/components/Video.tsx
@@ -3,7 +3,7 @@ import "./video.css"
 
 const VideoPlayer = () => {
   return (
-    <video width="1920" height="1080" playsInline autoPlay muted className="video-container video">
+    <video playsInline autoPlay muted className="video-container video">
       <source src="Hexagon Sketch Logo_1080p (1).mp4" type="video/mp4" /> 
      
       Your browser does not support the video tag.
