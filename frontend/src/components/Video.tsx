a602403560e1b9ff34e30cfb7ef4e3e7cadb2a7e
Nimrod Schatzman <nimrodschatzman5@gmail.com>
2025-03-03 20:14:55 +0200

    Prepare for Heroku deployment

--- frontend/src/components/Video.tsx
+++ frontend/src/components/Video.tsx
@@ -4,7 +4,8 @@ import "./video.css"
 const VideoPlayer = () => {
   return (
     <video width="640" height="360" playsInline autoPlay muted className="video">
-      {window.innerWidth<941 ?  <source src="Hexagon Sketch Logo_1080p.mp4" type="video/mp4" /> :  <source src="Hexagon Sketch Logo_1080p (1).mp4" type="video/mp4" />}
+      {window.innerWidth>941 &&  <source src="Hexagon Sketch Logo_1080p.mp4" type="video/mp4" /> }
+      {window.innerWidth<941 && <source src="Hexagon Sketch Logo_1080p (1).mp4" type="video/mp4" />}
      
       Your browser does not support the video tag.
     </video>
