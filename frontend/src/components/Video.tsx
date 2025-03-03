90f53dcf9113ccc0a09029b0fa29608d67c66538
Nimrod Schatzman <nimrodschatzman5@gmail.com>
2025-03-03 21:25:26 +0200

    Prepare for Heroku deployment

--- frontend/src/components/Video.tsx
+++ frontend/src/components/Video.tsx
@@ -1,12 +1,35 @@
-import React from "react";
-import "./video.css"
+import React, { useState, useEffect } from "react";
+import "./video.css";
 
 const VideoPlayer = () => {
+  const [videoSource, setVideoSource] = useState("");
+
+  useEffect(() => {
+    // Function to update video based on screen width
+    const updateVideoSource = () => {
+      if (window.innerWidth > 941) {
+        setVideoSource("Hexagon Sketch Logo_1080p.mp4");
+      } else {
+        setVideoSource("Hexagon Sketch Logo_1080p (1).mp4");
+      }
+    };
+
+    updateVideoSource(); // Initial check
+    window.addEventListener("resize", updateVideoSource); // Listen for window resize
+
+    return () => {
+      window.removeEventListener("resize", updateVideoSource); // Cleanup on unmount
+    };
+  }, []);
+
   return (
-    <video width="640" height="360" playsInline autoPlay muted className="video-container video">
-      {window.innerWidth>941 &&  <source src="Hexagon Sketch Logo_1080p.mp4" type="video/mp4" /> }
-      {window.innerWidth<941 && <source src="Hexagon Sketch Logo_1080p (1).mp4" type="video/mp4" />}
-     
+    <video
+      playsInline
+      autoPlay
+      muted
+      className="video-container video"
+    >
+      {videoSource && <source src={videoSource} type="video/mp4" />}
       Your browser does not support the video tag.
     </video>
   );
