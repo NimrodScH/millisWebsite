0c7022179cf016bc89a3ca437ab7b863bb508225
Nimrod <nimrodschatzman5@gmail.com>
2025-04-16 16:35:56 +0300

    make it better

--- frontend/src/components/Project-Cards/Project-Card.tsx
+++ frontend/src/components/Project-Cards/Project-Card.tsx
@@ -19,7 +19,18 @@ const ProjectCard = (props: {
   onCardClick?: () => void;
 }) => {
   const location = useLocation();
+  const navigate = useNavigate();
   const [isMouseHover, setMouseHover] = useState(false);
+
+  const handleReadMoreClick = () => {
+    navigate("/projects#active", {
+      state: {
+        activeCardIndex: props.index,
+        isOpen: true,
+        index: props.index,
+      },
+    });
+  };
 
   return (
     <div
@@ -51,14 +62,14 @@ const ProjectCard = (props: {
               <div className="project-card-text">{props.text}</div>
             </div>
 
-            <HashLink
-              to={"/projects#active"}
-              state={{ activeCardIndex: props.index, isOpen: true, index: props.index }}
-              className="project-card-read-more-container-hovering"
-            >
-              <span className="project-card-hint">{props.hintLabel}</span>
-              <img src="left arrow.png" className="project-card-icon1" />
-            </HashLink>
+            <div
+  className="project-card-read-more-container-hovering"
+  onClick={handleReadMoreClick}
+>
+  <span className="project-card-hint">{props.hintLabel}</span>
+  <img src="left arrow.png" className="project-card-icon1" />
+</div>
+
           </>
         ) : (
           <>
