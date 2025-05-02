db4c3bff46e98fd7b216a3a04aec382aec46ed2c
Nimrod <nimrodschatzman5@gmail.com>
2025-05-02 10:59:38 +0300

    fix

--- frontend/src/components/Footer/FooterLinks.tsx
+++ frontend/src/components/Footer/FooterLinks.tsx
@@ -1,47 +1,35 @@
-import { Link } from "react-router-dom"
-import { HashLink } from "react-router-hash-link"
+import { Link } from "react-router-dom";
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "instant" });
+};
 
 const FooterLinks = () => {
-return(<>
-<div className="footer-links">
-                <div className="footer-container3">
-                  <span className="footer-text23">
-                    <span>פרטים</span>
-                    <span></span>
-                  </span>
-                  <HashLink
-                    to="/about#"
-                    className="footer-link"
-                  >
-                    אודותינו
-                  </HashLink>
-                  <HashLink
-                    to={"/contact#"}
-                    className="footer-link"
-                  >
-                    יצירת קשר
-                  </HashLink>
-                  <HashLink
-                    to={"/projects#"}
-                    className="footer-link"
-                  >
-                    פרוייקטים
-                  </HashLink>
-                  <HashLink
-                    to={"/questions#"}
-                    className="footer-link"
-                  >
-                    שאלות תשובות
-                  </HashLink>
-                  <HashLink
-                    onClick={() => window.scrollTo(0, 0)}
-                    to={"/articles#"}
-                    className="footer-link"
-                  >
-                    מאמרים
-                  </HashLink>
-                </div>
-              </div>
-</>)
-}
-export default FooterLinks
+  return (
+    <div className="footer-links">
+      <div className="footer-container3">
+        <span className="footer-text23">
+          <span>פרטים</span>
+          <span></span>
+        </span>
+        <Link to="/about" className="footer-link" onClick={scrollToTop}>
+          אודותינו
+        </Link>
+        <Link to="/contact" className="footer-link" onClick={scrollToTop}>
+          יצירת קשר
+        </Link>
+        <Link to="/projects" className="footer-link" onClick={scrollToTop}>
+          פרוייקטים
+        </Link>
+        <Link to="/questions" className="footer-link" onClick={scrollToTop}>
+          שאלות תשובות
+        </Link>
+        <Link to="/articles" className="footer-link" onClick={scrollToTop}>
+          מאמרים
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default FooterLinks;
